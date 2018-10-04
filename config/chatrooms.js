73a17bf3c4c1d87a5fa6b38b5891c240df2d35fa
tanmoyb <tanmoy.zadid@yahoo.com>
2018-10-04 18:56:09 +0300

    chat room draft

---
+++ config/chatrooms.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: 'Cricket',
+    image: 'chatrooms/cricket.jpg'
+  },
+  {
+    name: 'Football',
+    image: 'chatrooms/football.jpg'
+  },
+  {
+    name: 'Baseball',
+    image: 'chatrooms/baseball.jpg'
+  },
+  {
+    name: 'Hockey',
+    image: 'chatrooms/hockey.jpg'
+  }
+]
---
+++ config/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'Murad',
+    lastName: 'Tanmoy',
+    statusText: 'Hi how are you doing',
+    image: 'users/murad.jpg'
+  },
+  {
+    name: 'John',
+    lastName: 'Snow',
+    statusText: 'winter is coming.',
+    image: 'users/john.jpg'
+  },
+  {
+    name: 'arya',
+    lastName: 'Strak',
+    statusText: 'No one',
+    image: 'users/arya.jpg'
+  }
+]
